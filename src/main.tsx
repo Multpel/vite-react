5038f27328a1ab7f2c1484ce85d980707721e16d
Marcos Paiva <marcospaiva@multpel.local>
2025-07-18 18:29:42 -0300

    Fix: Importacao explicita de App.tsx em main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import MaintenanceApp from './App'; // Importe seu componente principal aqui
+import MaintenanceApp from './App.tsx'; // Importe seu componente principal aqui
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
