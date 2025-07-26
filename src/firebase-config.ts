bc606943efa14b4fe6f483ada3146e88cbfa38ed
Marcos Paiva <marcospaiva@multpel.local>
2025-07-26 19:01:28 -0300

    Corrigindo erro de variavel 'analytics' nao utilizada

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -19,7 +19,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 // Inicialize o Firestore e o exporte para ser usado em outros arquivos
 export const db = getFirestore(app); // <-- Adicione esta linha
 
